ec4bff150aa9197136e028458f80df90da9ccfd3
ddsuhaimi <dedisuhaimiacc@gmail.com>
2023-09-20 21:09:31 +0700

    ss

--- create-file.js
+++ create-file.js
@@ -1,24 +1,50 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Content for the new file
 
 // Get the current date and time
 const now = new Date();
 const year = now.getFullYear();
-const month = String(now.getMonth() + 1).padStart(2, '0');
-const day = String(now.getDate()).padStart(2, '0');
-const hours = String(now.getHours()).padStart(2, '0');
-const minutes = String(now.getMinutes()).padStart(2, '0');
-const seconds = String(now.getSeconds()).padStart(2, '0');
+const month = String(now.getMonth() + 1).padStart(2, "0");
+const day = String(now.getDate()).padStart(2, "0");
+const hours = String(now.getHours()).padStart(2, "0");
+const minutes = String(now.getMinutes()).padStart(2, "0");
+const seconds = String(now.getSeconds()).padStart(2, "0");
 
 // Format the current date and time as a string
 const currentTime = `${year}${month}${day}_${hours}${minutes}${seconds}`;
 
-const content = `${currentTime}This is the content of the new file.`;
-// Specify the file path with the current time
-const filePath = `newfile.txt`;
+const getData = async () => {
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json, text/javascript, */*; q=0.01");
+  myHeaders.append(
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+  // myHeaders.append("Cookie", "b4f32afb85f1ce645593ba6f1280af85=b658d177aa18f0b0d99815447de4c010; pdns=906454026.47873.0000");
 
-// Write the content to the new file
-fs.writeFileSync(filePath, content);
+  var s1MatBody =
+    "draw=1&columns%5B0%5D%5Bdata%5D=jabatan_nm&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=ins_nm&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=lokasi_nm&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=false&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=false&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=disable&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=false&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=jumlah_formasi&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=false&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=aksi&columns%5B7%5D%5Bname%5D=&columns%5B7%5D%5Bsearchable%5D=false&columns%5B7%5D%5Borderable%5D=false&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&start=0&length=999&search%5Bvalue%5D=&search%5Bregex%5D=false&tp=60&pen=5101050&jp=&ins=";
 
-console.log(`Created ${filePath} with content: "${content}"`);
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: s1MatBody,
+    redirect: "follow",
+  };
+
+  try {
+      const req = await fetch(
+        "https://sscasn.bkn.go.id/actionCariBuilder",
+        requestOptions
+      );
+      const jsonReq = await req.json();
+      const filePath = `s1matematika.json`;
+      fs.writeFileSync(filePath, JSON.stringify(jsonReq));
+  } catch (error) {
+    console.log("error", error)
+  }
+
+};
+
+getData();

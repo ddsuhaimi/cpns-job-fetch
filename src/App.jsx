79cdaa415a7f5e86b2122dcb841da4dd23704013
ddsuhaimi <dedisuhaimiacc@gmail.com>
2023-09-23 16:14:20 +0700

    te

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const COLUMNS = [
   {
     Header: '#',
     accessor: (values, i) => {
-      console.log(values)
       return i
     },
   },
@@ -34,11 +33,20 @@ const COLUMNS = [
     Header: 'Jlh Jur',
     accessor: (values) => values.pendidikan_nm.split("/").length,
   },
+  {
+    Header: 'Gaji',
+    accessor:  (values) => Number(values.gaji_min/1000000).toFixed(1) +"-"+Number(values.gaji_max/1000000).toFixed(1),
+  },
 ];
 function App() {
   const [data, setData] = useState([])
   const columns = useMemo(() => COLUMNS, []);
   const [jurusan, setJurusan] = useState("s1Matematika")
+  const [tipe, setTipe] = useState({
+    cpns: true,
+    pppkUmum: true,
+    pppkKhusus: false
+  })
   const tableInstance = useTable({ columns, data });
   // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
   useEffect(() => {
@@ -46,15 +54,21 @@ function App() {
       let response = null
       if (jurusan === "s1Matematika") {
         response = await fetch("https://raw.githubusercontent.com/ddsuhaimi/cpns-job-fetch/main/s1matematika.json")
-      } else if (jurusan === "d4TeknikInformatika"){
-        response = await fetch("https://raw.githubusercontent.com/ddsuhaimi/cpns-job-fetch/main/d4teknikinformatika.json") 
+      } else if (jurusan === "d4TeknikInformatika") {
+        response = await fetch("https://raw.githubusercontent.com/ddsuhaimi/cpns-job-fetch/main/d4teknikinformatika.json")
       }
       const jb = await response.json()
-      console.log("🚀 ~ file: App.jsx:40 ~ getData ~ jb:", jb)
-      setData(jb.data.filter(item => item.disable === "0"))
+      // console.log("🚀 ~ file: App.jsx:40 ~ getData ~ jb:", jb)
+      let filteredData = jb.data
+      filteredData = filteredData.filter(item => item.disable === "0")
+      const cpnsFilter = filteredData.filter(item => tipe.cpns && item.jp_nama === "CPNS")
+      const p3kUmumFilter = filteredData.filter(item => tipe.pppkUmum && item.jp_nama === "PPPK Teknis" && item.formasi_nm === "UMUM")
+      const p3kKhususFilter = filteredData.filter(item => tipe.pppkKhusus && item.jp_nama === "PPPK Teknis" && item.formasi_nm === "KHUSUS")
+      filteredData = [...cpnsFilter, ...p3kUmumFilter, ...p3kKhususFilter]
+      setData(filteredData)
     }
     getData()
-  }, [jurusan])
+  }, [jurusan, tipe])
 
   const {
     getTableProps,
@@ -77,11 +91,8 @@ function App() {
   }
 
   const { globalFilter } = state;
-
   return (
     <div className='text-xs'>
-
-
       <div className="mt-2 mb-4 search-container ">
         <label htmlFor="cars">Pilih jurusan:</label>
         <select onChange={(e) => setJurusan(e.target.value)} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
@@ -99,6 +110,18 @@ function App() {
           value={globalFilter || ''}
           onChange={(e) => setGlobalFilter(e.target.value)}
         /> */}
+        <label>
+          <input type="checkbox" checked={tipe.cpns} onChange={e => setTipe(tipe => ({ ...tipe, cpns: e.target.checked }))} />
+          CPNS
+        </label>
+        <label>
+          <input type="checkbox" checked={tipe.pppkUmum} onChange={e => setTipe(tipe => ({ ...tipe, pppkUmum: e.target.checked }))} />
+          PPPK Umum
+        </label>
+        <label>
+          <input type="checkbox" checked={tipe.pppkKhusus} onChange={e => setTipe(tipe => ({ ...tipe, pppkKhusus: e.target.checked }))} />
+          PPPK Khusus
+        </label>
       </div>
 
       <span>- klik di kolom table untuk sorting</span>
